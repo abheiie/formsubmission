795a9f28c00c8efb83cb06ff69911476d2cf6c8c
Abhishek Singh <colorles7@gmail.com>
2021-01-06 16:23:10 +0530

    Update settings.py

--- formsubmission/settings.py
+++ formsubmission/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'formsubmission.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'powerfusionit',
-            'USER': 'pfittest',
-            'PASSWORD': 'abc123abc!',
-            'HOST': 'powerfusionit-test-instance-1.cue9mgwr0v4w.us-west-2.rds.amazonaws.com',
-            'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'powerfusionit',
+#             'USER': 'pfittest',
+#             'PASSWORD': 'abc123abc!',
+#             'HOST': 'powerfusionit-test-instance-1.cue9mgwr0v4w.us-west-2.rds.amazonaws.com',
+#             'PORT': '3306',
+#     }
+# }
 
 
 
